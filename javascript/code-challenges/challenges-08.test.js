cd754b934fbcb13ff46081599069ba860d5994df
Martha Quintanilla-Ramirez <marthaquin21@gmail.com>
2022-06-03 20:18:44 -0700

    updated syntax

--- javascript/code-challenges/challenges-08.test.js
+++ javascript/code-challenges/challenges-08.test.js
@@ -127,9 +127,9 @@ CHALLENGE 6
 Write a function named citiesAtoJ that takes in an array of city names and uses a regular expression pattern to return a new array containing any cities that begin with the letters A through J, inclusive.
 ------------------------------------------------------------------------------------------------ */
 
-const citiesAtoJ = (arr) => {
-  const patter = /^
-};
+// const citiesAtoJ = (arr) => {
+//   const patter = /^
+// };
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 7 - Stretch Goal
